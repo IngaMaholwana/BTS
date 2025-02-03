f93e2cae798654d146e217cd5a625b1cd60f3229
Inga Maholwana <imaholwana023@student.wethinkcode.co.za>
2025-02-03 14:51:36 +0200

    made a class and set up the objests for setting up the root and and left and right node objects

---
+++ Node.rb
@@ -0,0 +1,19 @@
+class Node
+  include Comparable
+
+  attr_accessor :data, :left, :right
+
+  def initialize(data)
+    @data = data
+    @left = nil
+    @right = nil
+  end
+
+  def <=>(other)
+    @data <=> other.data
+  end
+
+  def children?
+    @left.nil? && @right.nil? ? false : true
+  end
+end
